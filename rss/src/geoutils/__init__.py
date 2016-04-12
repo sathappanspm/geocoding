af282f168e9464da010407c9f7bb59e65fb1b66b
sathappanspm <sathappan.spm@gmail.com>
2016-04-12 16:20:40 -0400

    smart_open func added

--- rss/src/geoutils/__init__.py
+++ rss/src/geoutils/__init__.py
@@ -35,6 +35,43 @@ def encode(s):
     except:
         return s
 
+# code from smart_open package https://github.com/piskvorky/smart_open
+def make_closing(base, **attrs):
+    """
+    Add support for `with Base(attrs) as fout:` to the base class if it's missing.
+    The base class' `close()` method will be called on context exit, to always close the file properly.
+    This is needed for gzip.GzipFile, bz2.BZ2File etc in older Pythons (<=2.6), which otherwise
+    raise "AttributeError: GzipFile instance has no attribute '__exit__'".
+    """
+    if not hasattr(base, '__enter__'):
+        attrs['__enter__'] = lambda self: self
+    if not hasattr(base, '__exit__'):
+        attrs['__exit__'] = lambda self, type, value, traceback: self.close()
+    return type('Closing' + base.__name__, (base, object), attrs)
+
+
+# code from smart_open package https://github.com/piskvorky/smart_open
+def smart_open(fname, mode='rb'):
+    """
+    Stream from/to local filesystem, transparently (de)compressing gzip and bz2
+    files if necessary.
+    """
+    _, ext = os.path.splitext(fname)
+
+    if ext == '.bz2':
+        PY2 = sys.version_info[0] == 2
+        if PY2:
+            from bz2file import BZ2File
+        else:
+            from bz2 import BZ2File
+        return make_closing(BZ2File)(fname, mode)
+
+    if ext == '.gz':
+        from gzip import GzipFile
+        return make_closing(GzipFile)(fname, mode)
+
+    return open(fname, mode)
+
 
 class GeoData(object):
     def __init__(self):
